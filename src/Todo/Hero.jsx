efeb4308e735ce05dcc5952a12e56aa6a7125c05
Devanshukumawat <128327394+Devanshukumawat@users.noreply.github.com>
2025-04-04 14:21:22 +0530

    Add files via upload

---
+++ src/Todo/Hero.jsx
@@ -0,0 +1,15 @@
+import "./hero.css";
+
+function TODOHero() {
+  return (
+    <section className="todohero_section">
+      <div>
+        <p className="text_large">Task Done</p>
+        <p className="text_small">Keep it up</p>
+      </div>
+      <div>2/6</div>
+    </section>
+  );
+}
+
+export default TODOHero;
